8dfdd744b295f1402e72da354dba2f56becf90bb
Mohammed1000000000000000000 <84746256+Mohammed1000000000000000000@users.noreply.github.com>
2021-06-01 17:15:06 +1200

    Create Rules and welcoming_v1.py

---
+++ Rules and welcoming_v1.py
@@ -0,0 +1,11 @@
+status = (input("Do You want to see the rules Y/N"))
+
+if status == 'y' or status =='Y' or status =='Yes' :
+    print("You will be given a question and Multiple ansers\nand you have to choose the right anser\nPles anser the question with a,b,c or d and enjoy your time : ")
+
+else:
+    print("You may continue without the rules")
+    exit()
+
+ 
+    

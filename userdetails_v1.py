34f0278720389b3fb1fcfaf5a041fce898dc9df3
Mohammed1000000000000000000 <84746256+Mohammed1000000000000000000@users.noreply.github.com>
2021-06-01 17:10:08 +1200

    Create userdetails_v1.py

---
+++ userdetails_v1.py
@@ -0,0 +1,12 @@
+#Asking ueser detaeles
+
+print ("Welcom to the Quiz")
+
+
+name = input("what is your name  name : ")
+    
+   
+
+Age = input ("What is yuor age ")
+
+print ("Halow your Name is",name, "and yuor age is",Age)

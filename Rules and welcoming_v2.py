dc7cab9e0f074ce1bf10dac050cbfcae72bd90ef
Mohammed1000000000000000000 <84746256+Mohammed1000000000000000000@users.noreply.github.com>
2021-06-01 17:16:07 +1200

    Create Rules and welcoming_v2.py

---
+++ Rules and welcoming_v2.py
@@ -0,0 +1,8 @@
+rule = input("Please enter y if you wamt to read the rules\nor any other key if you dont want to read :")
+
+if rule ==  'Y' or rule  ==  'y' or  rule == 'Yes' or rule == 'yes':
+    print("You will be given a question and Multiple ansers\nand you have to choose the right anser\nPles anser the question with a,b,c or d and enjoy your time : ")
+    
+else:
+    print("enjoy yor time")
+   

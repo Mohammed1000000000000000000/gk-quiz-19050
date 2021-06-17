6001af80a43f7a92ca71d6f662d83a013fc25eb6
Mohammed1000000000000000000 <84746256+Mohammed1000000000000000000@users.noreply.github.com>
2021-06-17 13:26:25 +1200

    Update assembled_v2.py

--- assembled_v2.py
+++ assembled_v2.py
@@ -36,13 +36,25 @@ gkquiz = [
 shuffle(gkquiz)
 
 
-#Name 
+#Name Age 
 print ("Welcome to the Quiz")
-while True:     
+
+while True:
     name = input("what is your name : ")
-    if name.replace(' ','').isalpha():        
+    if name.replace(' ','').isalpha():
         break
-    print("q.py  characters A-Z only")
+    print("please enter characters A-Z onle")
+
+def age():
+    while True:
+        age = input("\nPlease enter your age : ")
+        if age.replace(' ','').isnumeric():#using replace to allow spaces after the answer
+            if 6< int(age)<101: #allowing age till 7 to 100 only
+                break
+            else:
+                print('You need to be 7 to 100')
+        else:
+            print("The data type you have used is invalid data type.\n")
 #Rules
 rule = input("Please enter y if you wamt to read the rules\nor any other key if you dont want to read :")
 
